3fdd83a0b458f261b309932cd82237341af0fbcc
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-01-29 20:11:05 +0300

    Update Swicth.java

--- beOnMax/src/Swicth.java
+++ beOnMax/src/Swicth.java
@@ -2,18 +2,50 @@ import java.sql.SQLOutput;
 
 public class Swicth {
     public static void main(String[] args) {
-        String Month = "СЕно";
+        main2();
+        String Month = "Декабрь";
         String[] Time = {"Winter","Spring","Summer","Autumn"};
         String TimeName="";
-        if (Month == "Декабрь" || Month == "Январь" || Month == "Февраль") {
+        if (Month.equals("Декабрь")|| Month.equals("Январь") || Month.equals("Февраль")) {
             TimeName=Time[0];
-        } else if (Month == "Март" || Month == "Апрель" || Month == "Май") {
+        } else if (Month.equals("Март") || Month.equals("Апрель") || Month.equals("Май")) {
             TimeName=Time[1];
-        } else if (Month == "Июнь" || Month == "Июль" || Month == "Август") {
+        } else if (Month.equals("Июнь") || Month.equals("Июль") || Month.equals("Август")) {
             TimeName=Time[2];
-        } else if (Month == "Сентябрь" || Month == "Октябрь" || Month == "Ноябрь") {
+        } else if (Month.equals("Сентябрь") || Month.equals("Октябрь") || Month.equals("Ноябрь")) {
             TimeName = Time[3];
         } else TimeName="Нет такого месяца";
+        //System.out.println(TimeName);
+    }
+
+    public static void main2() {
+        String Month = "Декабрь";
+        String[] Time = {"Winter","Spring","Summer","Autumn"};
+        String TimeName="";
+        switch (Month) {
+            case "Январь":
+            case "Декабрь":
+            case "Февраль":
+                TimeName= Time[0];
+                break;
+            case "Март":
+            case "Апрель":
+            case "Май":
+                TimeName= Time[1];
+                break;
+            case "Июнь":
+            case "Июль":
+            case "Август":
+                TimeName= Time[2];
+                break;
+            case "Сентябрь":
+            case "Октябрь":
+            case "Ноябрь":
+                TimeName= Time[3];
+                break;
+            default:
+                TimeName= "Такого месяца не существует";
+        }
         System.out.println(TimeName);
     }
 }
