c93f1b7cc936182d2512dee2343f8c3ee1962bcc
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-02-04 22:44:20 +0300

    модификаторы

--- beOnMax/src/OOP/Box/Box.java
+++ beOnMax/src/OOP/Box/Box.java
@@ -1,24 +1,41 @@
 package OOP.Box;
 
-public class Box {
-    double length;
-    double width;
-    double height;
+ class Box {
+    private double length;
+    private double width;
+    private double height;
 
-    public Box(double length, double width, double height) {
+    Box(double length, double width, double height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
-    public Box (Box box1,Box box2) {
+    Box (Box box1,Box box2) {
         length = box1.length+box2.length;
         width = box1.width+ box2.width;
         height = box1.height + box2.height;
     }
-    Box sumBox (Box box) {
-        return new Box (length+box.length,width+box.width,height+box.height);
+
+     public double getLength() {
+         return length;
+     }
+
+     public double getWidth() {
+         return width;
+     }
+
+     public double getHeight() {
+         return height;
+     }
+
+     Box sumBox (Box box) {
+        return new Box (getLength()+box.getLength(),getWidth()+box.getWidth(),
+                getHeight()+box.getHeight());
     }
     double volume () {
         return length*height*width;
+    }
+    void boxInfo () {
+        System.out.println("Длина: " + length + "; Высота: " + height + "; Ширина: " + width);
     }
 }
